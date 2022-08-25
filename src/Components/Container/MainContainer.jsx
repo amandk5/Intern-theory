baf53c1570d685eb69153157206ed560b7d7a38c
amandk5 <adkofficial01@gmail.com>
2022-08-25 09:02:46 +0530

    day 2 updated

--- src/Components/Container/MainContainer.jsx
+++ src/Components/Container/MainContainer.jsx
@@ -1,14 +1,19 @@
 import { Box, Button, Container } from "@chakra-ui/react";
 import React from "react";
 import companies from "../../Images/companies.PNG";
-import l1 from "../../Images/l1.PNG"
-import l2 from "../../Images/l2.PNG"
-import l3 from "../../Images/l3.PNG"
-import l4 from "../../Images/l4.PNG"
-import l5 from "../../Images/l5.PNG"
-import l6 from "../../Images/l6.PNG"
-import l7 from "../../Images/l7.PNG"
-import l8 from "../../Images/l8.PNG"
+import l1 from "../../Images/l1.PNG";
+import l2 from "../../Images/l2.PNG";
+import l3 from "../../Images/l3.PNG";
+import l4 from "../../Images/l4.PNG";
+import l5 from "../../Images/l5.PNG";
+import l6 from "../../Images/l6.PNG";
+import l7 from "../../Images/l7.PNG";
+import l8 from "../../Images/l8.PNG";
+import u1 from "../../Images/upskill1.PNG";
+import u2 from "../../Images/upskill2.PNG";
+import u3 from "../../Images/upskill3.PNG";
+import { AiFillLinkedin, AiFillTwitterSquare } from "react-icons/ai";
+import { FaInstagramSquare, FaFacebookSquare } from "react-icons/fa";
 
 import "../../Styles.css";
 
@@ -62,13 +67,11 @@ export default function MainContainer() {
           </Box>
         </Box>
       </Container>
-      <Container
-        className="cont-2"
-        maxW="4x2"
-      >
+      <Container className="cont-2" maxW="4x2">
         <div>
           <h1>
-            PLETHORA OF OPPORTUNITIES <span style={{color:"#DF1E2E"}}>FOR YOU</span>
+            PLETHORA OF OPPORTUNITIES{" "}
+            <span style={{ color: "#DF1E2E" }}>FOR YOU</span>
           </h1>
           <hr />
           <p>
@@ -82,7 +85,10 @@ export default function MainContainer() {
         </div>
       </Container>
       <Container className="cont-3" maxW="4x2">
-        <h1>LET'S GET SOME <span style={{color:"#DF1E2E"}}>WORK EXPERIENCE</span></h1>
+        <h1>
+          LET'S GET SOME{" "}
+          <span style={{ color: "#DF1E2E" }}>WORK EXPERIENCE</span>
+        </h1>
         <div>
           <img src={`${l1}`} alt="marketing" />
           <img src={`${l2}`} alt="business dev" />
@@ -93,6 +99,87 @@ export default function MainContainer() {
           <img src={`${l7}`} alt="engineering" />
           <img src={`${l8}`} alt="all internships" />
         </div>
+      </Container>
+      <Container className="cont-4" maxW="4x2">
+        <h1>
+          HOW ABOUT <span style={{ color: "#DF1E2E" }}>UPSKILLING ?</span>
+        </h1>
+        <div>
+          <img src={`${u1}`} alt="stock trader" />
+          <img src={`${u2}`} alt="digital marketing" />
+          <img src={`${u3}`} alt="browse website" />
+        </div>
+      </Container>
+      <Container className="cont-5" maxW="4x2">
+        <div>
+          <img
+            src="https://assets.interntheory.com/creative/chart.png"
+            alt="creative chart"
+            style={{ maxWidth: "" }}
+          />
+        </div>
+        <div>
+          <h1>
+            PLETHORA OF OPPORTUNITIES{" "}
+            <span style={{ color: "#DF1E2E" }}>FOR YOU</span>
+          </h1>
+          <hr />
+          <p>
+            We are on a mission to connect employers to young talent all over
+            India. With our strong corporate network, it will no longer be a
+            challenge to find the right internship for you
+          </p>
+        </div>
+      </Container>
+      <Container className="cont-6" maxW="4x2">
+        SLIDESHOW WILL APPEAR HERE
+      </Container>
+      <Container className="cont-7-footer" maxW="4x2">
+        <div>
+          <div>
+            <FaFacebookSquare size="30" />
+            <AiFillLinkedin size="30" />
+            <AiFillTwitterSquare size="30" />
+            <FaInstagramSquare size="30" />
+          </div>
+          <div>
+            <p>BY PLACE</p>
+            <hr />
+            <p>BY PLACE Internships In Mumbai</p>
+            <p>Internships In Delhi</p>
+            <p>Internships In Bangalore</p>
+            <p>Internships In Pune</p>
+            <p>Internships In Hyderabad</p>
+          </div>
+          <div>
+            <p>BY PROFILE</p>
+            <hr />
+            <p>Marketing Internships</p>
+            <p>Business Development Internships</p>
+            <p>Content Writing Internships</p>
+            <p>Graphic Designing Internships</p>
+            <p>HR Internships</p>
+            <p>Engineering Internships</p>
+          </div>
+          <div>
+            <p>BY TYPE</p>
+            <hr />
+            <p>Full Time Internships</p>
+            <p>Part Time Internships</p>
+            <p>Work From Home Internships</p>
+          </div>
+        </div>
+        <div>
+          <p>REFUND & CANCELLATION POLICY</p>
+          <p>COURSES</p>
+          <p>TERMS AND CONDITIONS</p>
+          <p>PRIVACY POLICY</p>
+          <p>CONTACT US</p>
+          <p>ABOUT US</p>
+          <p>SITEMAP</p>
+        </div>
+        <hr />
+        <p>© 2015 - 2022 INTERNTHEORY. ALL RIGHTS RESERVED.</p>
       </Container>
     </div>
   );
